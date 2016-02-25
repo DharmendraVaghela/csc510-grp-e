3a760030449a4f61a1c9b311132158c20b80bb02
mandgerohit <mandge.rohit@gmail.com>
2016-02-24 23:18:17 -0500

    Added feature to detect figures

--- chrome_extension/findtext_contentscript.js
+++ chrome_extension/findtext_contentscript.js
@@ -1,6 +1,9 @@
 var arrayOfNumbers = [];
-alert("hi");
-var regex =  /\d*[/-]*[0-9][0-9][0-9][/ -]*[0-9][0-9][0-9][/ -]*[0-9][0-9][0-9][0-9][ ]*/g;
+var hash = {};
+//alert("hi");
+var regex =  /\w*[figure|Fig.|fig][ ][0-9]/gi;
+//var regex3 = /((https|http)?:\/\/.*\.(?:png|jpg))/i;
+//var regex4 = /([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i;
 newBody = document.body.innerHTML;
 var i = 0;
 do
@@ -11,6 +14,37 @@ do
     i++
 }
 while (temp)
+
+
+input_content=newBody;
+var matches = [];
+
+input_content.replace(/[^<]*(<a href="([^"]+)">([^<]+)<\/a>)/g, function () {
+    matches.push(Array.prototype.slice.call(arguments, 1, 4));
+});
+
+alert(matches.join("\n"));
+
+
+/*
+reg = new RegExp(
+    "[\\w/.]*(jpg|gif|png)(\\?[\\w=&]*)?",
+    "gi");
+*/
+/*
+reg = new RegExp(
+    "/([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))",
+    "gi");
+var result;
+var images = [];
+while ((result = reg.exec(newBody)) !== null) {
+    var res0 = result[0];
+    images.push(res0);
+}
+*/
+//alert(images.join("\n"))
+//alert(arrayOfNumbers.join("\n"));
+
 for (var i = 0; i < arrayOfNumbers.length; i++)
 {
     newBody = newBody.replace(arrayOfNumbers[i], "<a href='http://www.google.com'>" + arrayOfNumbers[i] + "</a>");
