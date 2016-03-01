6183ef50202a1f7ccdabe307ee7d32b762be9fcc
mandgerohit <mandge.rohit@gmail.com>
2016-02-29 23:48:28 -0500

    Optimized regex

--- chrome_extension_hover/findtext_contentscript.js
+++ chrome_extension_hover/findtext_contentscript.js
@@ -1,27 +1,41 @@
-var arrayOfNumbers = [];
+var arrayOfWords = [];
 
-var regex =  /\w*[figure|Fig.|fig][ ][0-9]/gi;
+//var regex =  /\w*[figure|Figure|fig|fig.][\s][0-9]/gi;
+var regex =  /(fig|figure|fig.)[\s][0-9]/gi;
 
 newBody = document.body.innerHTML;
+
+array = newBody.toString().match(regex);
+
+/*
 var i = 0;
 do
 {
-    temp = regex.exec(newBody);
+    temp = newBody.toString().match(regex);
     if (temp != null)
-        arrayOfNumbers[i] = temp;
+    {
+        alert(temp);
+        arrayOfWords[i] = temp;
+        
+    }
     i++
 }
 while (temp)
-
-var myArray=new Array(100);
-
-for (i=0; i <100; i++)
-    myArray[i]=new Array(2);
-
+*/
+var arrayOfWords=[];
+for (var i = 0; i < array.length; i++)
+{ 
+    if(arrayOfWords.indexOf(array[i])<0){
+        arrayOfWords.push(array[i]);
+    }
+    //alert(arrayOfWords[i]);
+}
+alert(arrayOfWords);
 var images = document.getElementsByTagName("img");
 
-//background: red; 
-var css = 'a>div { display: none; } a:hover>div { display: block; }',
+var anchors = document.getElementsByTagName("a");
+
+var css = 'a>div { display: none; } a:hover>div { display: block; }';
 head = document.head || document.getElementsByTagName('head')[0],
 style = document.createElement('style');
 style.type = 'text/css';
@@ -35,12 +49,17 @@ document.head.appendChild(style);
 
 newBody = document.body.innerHTML;
 
-for (var i = 0; i < arrayOfNumbers.length; i++)
-{
-    for(var j = 0; j < images.length; j++){
-        if(images[j].alt.indexOf(arrayOfNumbers[i])>-1)
+for (var i = 0; i < arrayOfWords.length; i++)
+{   
+    for(var j = 0; j < images.length; j++)
+    {   
+        if(images[j].alt.indexOf(arrayOfWords[i])>-1)
         {   
-            newBody = newBody.replace(arrayOfNumbers[i], "<a href="+images[j].src+">"+"<div><img src="+images[j].src+">"+"</img>"+"</div>"+ arrayOfNumbers[i] + "</a>");
+            //alert(arrayOfWords[i]);
+            var rep = "<a href="+images[j].src+">"+"<div><img src="+images[j].src+">"+"</img>"+"</div>"+ arrayOfWords[i] + "</a>";
+            newBody = newBody.replace(arrayOfWords[i], rep);
+            alert(arrayOfWords[i]);
+            alert(rep);
         }
     }
 }
