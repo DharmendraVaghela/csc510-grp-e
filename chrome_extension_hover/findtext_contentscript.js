176e5f544e58bfcf8e22f29091dc168ab95bb4ec
mandgerohit <mandge.rohit@gmail.com>
2016-03-01 19:53:38 -0500

    Implemented using hashmaps

--- chrome_extension_hover/findtext_contentscript.js
+++ chrome_extension_hover/findtext_contentscript.js
@@ -1,39 +1,45 @@
-var arrayOfWords = [];
+var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
+console.log(newURL);
+console.time();
 
-//var regex =  /\w*[figure|Figure|fig|fig.][\s][0-9]/gi;
+var array = [];
+var hash = new Object();
+
 var regex =  /(fig|figure|fig.)[\s][0-9]/gi;
 
 newBody = document.body.innerHTML;
 
 array = newBody.toString().match(regex);
 
-/*
-var i = 0;
-do
-{
-    temp = newBody.toString().match(regex);
-    if (temp != null)
-    {
-        alert(temp);
-        arrayOfWords[i] = temp;
-        
-    }
-    i++
-}
-while (temp)
-*/
 var arrayOfWords=[];
 for (var i = 0; i < array.length; i++)
 { 
     if(arrayOfWords.indexOf(array[i])<0){
-        arrayOfWords.push(array[i]);
+        arrayOfWords.push(array[i].toString());
     }
-    //alert(arrayOfWords[i]);
 }
-alert(arrayOfWords);
+console.log(arrayOfWords);
+
 var images = document.getElementsByTagName("img");
 
-var anchors = document.getElementsByTagName("a");
+for (var i = 0; i < arrayOfWords.length; i++)
+{
+    for(var j = 0; j < images.length; j++)
+    {
+        //var exp = /\d+/gi;
+        //var num = arrayOfWords[i].toString().match(exp)[0];
+        if(images[j].alt.indexOf(arrayOfWords[i])>-1)
+        {
+            if(images[j].src!=undefined){
+                hash[arrayOfWords[i]] = images[j].src;
+                //alert(hash[arrayOfWords[i]]);
+            }
+        }
+    }
+}
+
+console.log(hash);
+//var anchors = document.getElementsByTagName("a");
 
 var css = 'a>div { display: none; } a:hover>div { display: block; }';
 head = document.head || document.getElementsByTagName('head')[0],
@@ -47,21 +53,16 @@ if (style.styleSheet){
 
 document.head.appendChild(style);
 
-newBody = document.body.innerHTML;
-
 for (var i = 0; i < arrayOfWords.length; i++)
-{   
-    for(var j = 0; j < images.length; j++)
-    {   
-        if(images[j].alt.indexOf(arrayOfWords[i])>-1)
-        {   
-            //alert(arrayOfWords[i]);
-            var rep = "<a href="+images[j].src+">"+"<div><img src="+images[j].src+">"+"</img>"+"</div>"+ arrayOfWords[i] + "</a>";
+{    
+    var img = hash[arrayOfWords[i]];
+        if(img != undefined)
+        {
+            var rep = "<a href="+img+">"+"<div><img src="+img+">"+"</img>"+"</div>"+ arrayOfWords[i] + "</a>";
             newBody = newBody.replace(arrayOfWords[i], rep);
-            alert(arrayOfWords[i]);
-            alert(rep);
+            console.log(arrayOfWords[i]);
+            console.log(rep);
         }
-    }
 }
 
 document.body.innerHTML = newBody;
